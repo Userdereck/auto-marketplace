cf1df54d50c167579f7693fccb00d91d85a31da9
UserDereck <ingalexandermejiag@gmail.com>
2025-10-07 22:45:13 -0400

    feat: añadir backend de la API

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,15 @@
+// backend/ecosystem.config.js
+module.exports = {
+  apps: [{
+    name: 'autobid-api',
+    script: './src/app.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 5000
+    }
+  }]
+};
